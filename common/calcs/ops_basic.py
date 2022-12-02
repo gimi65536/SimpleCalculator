cf47cef8e86106526b99608905138191883e0137
Wayne Zeng <wayne@gimi65536.xyz>
2022-12-02 17:31:49 +0900

    Add numeric unary operators and Boolean operators

--- common/calcs/ops_basic.py
+++ common/calcs/ops_basic.py
@@ -1,5 +1,5 @@
 from .types import *
-from .ops import BinaryOperator
+from .ops import BinaryOperator, UnaryOperator
 
 class PlusOperator(BinaryOperator):
 	def eval(self):
@@ -78,3 +78,84 @@ class ModuloOperator(BinaryOperator):
 			return NumberConstant(a.value % b.value)
 		else:
 			raise ValueError('Invalid type division')
+
+class PositiveOperator(UnaryOperator):
+	def eval(self):
+		a = self.extract_constant(*self.eval_operands())[0]
+
+		if a.is_number:
+			return a
+		else:
+			raise ValueError('Only positive number')
+
+class NegativeOperator(UnaryOperator):
+	def eval(self):
+		a = self.extract_constant(*self.eval_operands())[0]
+
+		if a.is_number:
+			return NumberConstant(-a.value)
+		else:
+			raise ValueError('Only negative number')
+
+class NotOperator(UnaryOperator):
+	def eval(self):
+		a = self.extract_constant(*self.eval_operands())[0]
+
+		if a.is_bool:
+			return BooleanConstant(not a.value)
+		else:
+			a = a.to_bool()
+			return BooleanConstant(not a.value)
+
+class _BinaryBoolOperator(BinaryOperator):
+	def eval(self):
+		a, b = self.extract_constant(*self.eval_operands())
+
+		a, b = a.to_bool(), b.to_bool()
+		return BooleanConstant(self._logic(a.value, b.value))
+
+	def _logic(self, a: bool, b: bool, /):
+		raise NotImplementedError
+
+class AndOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return a and b
+
+class OrOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return a or b
+
+class ImplOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return not a or b
+
+class NimplOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return a and not b
+
+class XorOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return a != b
+
+class IffOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return a == b
+
+class NandOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return not(a and b)
+
+class NorOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return not(a or b)
+
+# The below two operators are rarely used and I will not use them
+# Of course these two ops are here to be invoked for free
+
+class ConverseImplOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return a or not b
+
+class ConverseNimplOperator(_BinaryBoolOperator):
+	def _logic(self, a, b, /):
+		return not a and b
