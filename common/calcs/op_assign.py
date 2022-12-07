15f48e1b37336ee7c1fa62c9a5aa581af2c54052
Wayne Zeng <wayne@gimi65536.xyz>
2022-12-07 22:54:20 +0900

    Basic assignment

---
+++ common/calcs/op_assign.py
@@ -0,0 +1,13 @@
+from .types import *
+from .ops import BinaryOperator
+
+class AssignOperator(BinaryOperator):
+	def eval(self, mapping):
+		a, b = self.eval_operands(mapping)
+		b = self.extract_constant(b)[0]
+
+		if not a.is_lvalue:
+			raise ValueError('Only applies for variables')
+
+		a.content = b.without_dummy()
+		return a
