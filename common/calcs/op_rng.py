3a1dd833ebe0a0deae06c54b2a49e48652f1bdfd
Wayne Zeng <wayne@gimi65536.xyz>
2022-12-07 19:19:31 +0900

    Rewrite op_rng

--- common/calcs/op_rng.py
+++ common/calcs/op_rng.py
@@ -16,11 +16,8 @@ class RandomWithSeedOperator(UnaryOperator):
 
 		return NumberConstant(Float(rng.random()))
 
-class RandomIntOperator(TernaryOperator):
-	ary = 3
-	def eval(self, mapping):
-		s, a, b = self.extract_constant(*self.eval_operands(mapping))
-
+class _RandomIntOperator:
+	def _eval(self, a: Constant, b: Constant, seed = None):
 		if (a.is_number and b.is_number) and (a.value.is_real and b.value.is_real):
 			if a.value > b.value:
 				a, b = b, a
@@ -29,34 +26,67 @@ class RandomIntOperator(TernaryOperator):
 			if na > nb:
 				raise ValueError('Not valid interval: no integer is included')
 
-			if not s.is_dummy:
-				rng.seed(str(s.value))
+			if seed is not None and not seed.is_dummy:
+				rng.seed(str(seed.value))
 
 			return NumberConstant(Integer(rng.randint(int(na), int(nb))))
 
 		raise ValueError('Only apply real numbers as input')
 
-class RandomRealOperator(TernaryOperator):
+class RandomIntOperator(BinaryOperator, _RandomIntOperator):
+	def eval(self, mapping):
+		a, b = self.extract_constant(*self.eval_operands(mapping))
+
+		return self._eval(a, b)
+
+class RandomIntWithSeedOperator(TernaryOperator, _RandomIntOperator):
 	def eval(self, mapping):
 		s, a, b = self.extract_constant(*self.eval_operands(mapping))
 
+		return self._eval(a, b, s)
+
+class _RandomRealOperator:
+	def _eval(self, a: Constant, b: Constant, seed = None):
 		if (a.is_number and b.is_number) and (a.value.is_real and b.value.is_real):
-			if not s.is_dummy:
-				rng.seed(str(s.value))
+			if seed is not None and not seed.is_dummy:
+				rng.seed(str(seed.value))
 
 			return NumberConstant(Number(rng.uniform(a.value, b.value)))
 
 		raise ValueError('Only apply real numbers as input')
 
-class RandomComplexOperator(Operator):
-	ary = 5
+class RandomRealOperator(BinaryOperator, _RandomRealOperator):
 	def eval(self, mapping):
-		s, a, b, c, d = self.extract_constant(*self.eval_operands(mapping))
+		a, b = self.extract_constant(*self.eval_operands(mapping))
 
+		return self._eval(a, b)
+
+class RandomRealWithSeedOperator(TernaryOperator, _RandomRealOperator):
+	def eval(self, mapping):
+		s, a, b = self.extract_constant(*self.eval_operands(mapping))
+
+		return self._eval(a, b, s)
+
+class _RandomComplexOperator:
+	def _eval(self, a: Constant, b: Constant, c: Constant, d: Constant, seed = None):
 		if (a.is_number and b.is_number) and (a.value.is_real and b.value.is_real and c.value.is_real and d.value.is_real):
-			if not s.is_dummy:
-				rng.seed(str(s.value))
+			if seed is not None and not seed.is_dummy:
+				rng.seed(str(seed.value))
 
 			return NumberConstant(random_complex_number(a.value, c.value, b.value, d.value))
 
 		raise ValueError('Only apply real numbers as input')
+
+class RandomComplexWithSeedOperator(Operator, _RandomComplexOperator):
+	ary = 4
+	def eval(self, mapping):
+		a, b, c, d = self.extract_constant(*self.eval_operands(mapping))
+
+		return self._eval(a, b, c, d)
+
+class RandomComplexWithSeedOperator(Operator, _RandomComplexOperator):
+	ary = 5
+	def eval(self, mapping):
+		s, a, b, c, d = self.extract_constant(*self.eval_operands(mapping))
+
+		return self._eval(a, b, c, d, s)
