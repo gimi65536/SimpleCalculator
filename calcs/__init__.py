3fba425200151675ccb3e9f01c31de710c6ac594
gimi65536 <gimi65536@hotmail.com>
2022-12-10 21:57:46 +0900

    Add give_advanced_parser() into the package

--- calcs/__init__.py
+++ calcs/__init__.py
@@ -61,3 +61,62 @@ def give_basic_parser():
 	]
 
 	return Parser(default_prefix_ops, default_postfix_ops, default_precedence_table)
+
+def give_advanced_parser():
+	default_precedence_table = {
+		9: PrecedenceLayer(Associability.RIGHT,
+			*OperatorInfo.factory(op_num.PowOperator, '**', '^')
+		),
+		10: PrecedenceLayer(Associability.LEFT,
+			OperatorInfo(op_basic.MultipleOperator, '*'),
+			OperatorInfo(op_basic.DivideOperator, '/'),
+			OperatorInfo(op_basic.IntegerDivideOperator, '//'),
+			OperatorInfo(op_basic.ModuloOperator, '%'),
+		),
+		20: PrecedenceLayer(Associability.LEFT,
+			OperatorInfo(op_basic.PlusOperator, '+'),
+			OperatorInfo(op_basic.MinusOperator, '-'),
+			OperatorInfo(op_basic.ConcatOperator, '.'),
+		),
+		70: PrecedenceLayer(Associability.LEFT,
+			OperatorInfo(op_basic.LessOperator, '<'),
+			OperatorInfo(op_basic.LeOperator, '<='),
+			OperatorInfo(op_basic.GreaterOperator, '>'),
+			OperatorInfo(op_basic.GeOperator, '>='),
+		),
+		80: PrecedenceLayer(Associability.LEFT,
+			OperatorInfo(op_basic.EqualOperator, '=='),
+			OperatorInfo(op_basic.NonequalOperator, '!='),
+		),
+		110: PrecedenceLayer(Associability.RIGHT,
+			*OperatorInfo.factory(op_basic.AndOperator, '&&', 'and'),
+			*OperatorInfo.factory(op_basic.OrOperator, '||', 'or'),
+			OperatorInfo(op_basic.ImplOperator, '->'),
+			OperatorInfo(op_basic.XorOperator, 'xor'),
+			OperatorInfo(op_basic.IffOperator, '<->'),
+		),
+	}
+
+	default_prefix_ops = [
+		OperatorInfo(op_basic.PositiveOperator, '+'),
+		OperatorInfo(op_basic.NegativeOperator, '-'),
+		*OperatorInfo.factory(op_basic.NotOperator, '~', 'not', '!'),
+		OperatorInfo(op_num.AbsOperator, 'abs'),
+		OperatorInfo(op_rng.RandomOperator, 'random'),
+		OperatorInfo(op_rng.RandomWithSeedOperator, 'random'),
+		OperatorInfo(op_str.LengthOperator, 'len'),
+		OperatorInfo(op_str.StrictSymParseOperator, 'parse'),
+		OperatorInfo(op_utils.PrintOperator, 'print'),
+		OperatorInfo(op_utils.PassOperator, 'pass'),
+		OperatorInfo(op_utils.DummizeOperator, 'dummy'),
+		OperatorInfo(op_utils.DedummizeOperator, 'solid'),
+		OperatorInfo(op_utils.ReverseOperator, 'reverse'),
+		*OperatorInfo.factory(op_utils.RaiseOperator, 'raise', 'throw'),
+		OperatorInfo(op_utils.DecimalPointOperator, '.'),
+	]
+
+	default_postfix_ops = [
+		OperatorInfo(op_num.FactorialOperator, '!'),
+	]
+
+	return Parser(default_prefix_ops, default_postfix_ops, default_precedence_table)
